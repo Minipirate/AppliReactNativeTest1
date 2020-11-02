ac6cd67d26c97d5c77e3916e276a31375e08fa92
Minipirate <nessti@live.fr>
2020-11-02 14:42:43 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {Text, AppRegistry} from 'react-native';
+import App from './App';
+import {AppliMiniPirate} from './app.json';
+
+AppRegistry.registerComponent('AppliMiniPirate', () => App);
